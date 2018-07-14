31f69b332d35c495cd3795c4c44e2dc7829c4d2b
pingjiang <pingjiang1989@gmail.com>
2018-07-14 11:56:30 +0800

    add loremapp

---
+++ src/main/java/com/huawei/citu/loremapp/LoremappApplication.java
@@ -0,0 +1,12 @@
+package com.huawei.citu.loremapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoremappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoremappApplication.class, args);
+	}
+}
